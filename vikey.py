e893eeb42cc56ebc9a81fbf0a6ecb0a1c816866b
vignesh-beep <52270714+vignesh-beep@users.noreply.github.com>
2019-08-01 10:33:45 +0530

    Create vikey.py

---
+++ vikey.py
@@ -0,0 +1,7 @@
+x = 1.626
+y = -2.31
+print("\nOriginal Number: ", x)
+print("Formatted Number with sign: "+"{:+.2f}".format(x));
+print("Original Number: ", y)
+print("Formatted Number with sign: "+"{:+.2f}".format(y));
+print()
